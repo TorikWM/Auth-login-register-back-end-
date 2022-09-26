e4091a4f42c2f50694efa7f58ec331df6c0a02a6
Med Malek Bouraoui <malek.bouraoui.1234@gmail.com>
2022-09-26 17:26:00 +0100

    first

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app=express()
+const connectDB=require("./config/connection")
+const authRouter=require("./routes/auth")
+
+// connect with db
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use('/api/auth',authRouter);
+
+const port=5000;
+app.listen(port, err=> err ? console.log(err):console.log(`Server started on port ${port}`));
